6f7c66744dda596ab8683f67eabbe78845df618a
mor ashush <morsopher@gmail.com>
2022-05-25 11:44:58 +0300

    final change in function

--- exams/ex01/message.cpp
+++ exams/ex01/message.cpp
@@ -18,12 +18,20 @@ int send_a_message(struct message const& a_msg, TCPClientSocket a_client)
 	char* packet = new char[sizeof(struct message)];
 	char* ptr = packet;
 
+	*ptr = sizeof(int);
+	++ptr;
 	*ptr = a_msg.m_id;
+	
 	ptr += sizeof(int);
 
+	*ptr = 2 * sizeof(int);
+	++ptr;
 	*ptr = a_msg.m_speed;
+	
 	ptr += 2 * sizeof(int);
 
+	*ptr = 2 * sizeof(int);
+	++ptr;
 	*ptr = a_msg.m_altitude;
 
 	return a_client.send(packet);
