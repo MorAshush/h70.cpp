ccdcd5dae6d9a7e37e7e60569edaf432fda5d367
mor ashush <morsopher@gmail.com>
2022-05-25 11:07:20 +0300

    current state of exam01

--- exams/ex01/client_socket.hpp
+++ exams/ex01/client_socket.hpp
@@ -1,31 +1,38 @@
 #ifndef CLIENT_SOCKET_HPP
 #define CLIENT_SOCKET_HPP
 
-#include <iostream>
-#include <cassert>
-#include <sys/socket.h> 
 
 
-class ClientSocket
+class TCPClientSocket
 {
-public:    
-	ClientSocket();
 
-	int connect(char* a_serverAddr);
+public:  
+/**
+ * @brief constructor - opens a socket (and connects it to server)
+ * @param server address to conncet client to
+ */  
+	ClientSocket(char* a_serverAddr);
 
+/**
+ * @brief sends a packet from client to server. (adds TCP header to the packet)
+ * @param a ptr to message
+ * @return 0 if sent successfully, or -1 if failed
+ */ 
 	int send(char* a_data);
 
-	int recieve();
+/**
+ * @brief recieve a packet from server to client.
+ * @param a ptr to a buffer that recieves the message
+ * @return 0 if recieved successfully, or -1 if failed
+ */ 
+	int recieve(char* m_buffer);
 
-	int get_socket();
+//	int get_socket();
 
 private:
 	int m_socket;
-	char* m_buffer; // should define a max size?
+	char* m_serverAddr;
 };
-
-
-
 
 
 
---
+++ exams/ex01/message.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cassert>
+#include <sys/socket.h>
+
+#include "client_socket.hpp"
+
+
+struct message
+{
+	int m_id;
+	double m_speed;
+	double m_altitude;
+};
+
+
+int send_a_message(struct message const& a_msg, TCPClientSocket a_client)
+{
+	char* packet = new char[sizeof(struct message)];
+	char* ptr = packet;
+
+	*ptr = a_msg.m_id;
+	ptr += sizeof(int);
+
+	*ptr = a_msg.m_speed;
+	ptr += 2 * sizeof(int);
+
+	*ptr = a_msg.m_altitude;
+
+	return a_client.send(packet);
+}
